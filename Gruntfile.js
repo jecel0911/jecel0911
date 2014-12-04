d93c150b843ec8eaa068096b419eef92b337780f
Jeremias Ceciliano <jecel0911@gmail.com>
2014-12-04 00:05:54 -0600

    initiating new version of page with grunt, stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   
   	//configuration
 	grunt.initConfig({
-		//Minified HTML
+		  //Minified HTML
   		htmlmin: {                                     		// Task
     		dist: {                                      	// Target
       			options: {                                 	// Target options
@@ -10,28 +10,53 @@ module.exports = function(grunt) {
         			collapseWhitespace: true
       			},
       			files: {                                   	// Dictionary of files
-        			'dist/index.html': 'index.html',     	// 'destination': 'source'
-        			'dist/contact.html': 'demo.html'
+        			'dist/index.html': 'index.html'     	// 'destination': 'source'
+        			//,'dist/contact.html': 'demo.html'
       			}
     		}
   		},
+      //Minified js
   		uglify: {
     		my_target: {
       			files: {
-        			'dist/js/main.min.js': ['js/init.js', 
-        								    'js/scripts.js']
-      			}
+        			'dist/js/init.min.js': ['js/init.js'
+        								              ,'js/modernizr.js'
+                                     ]
+             /*,'dist/js/main.min.js': ['js/init.js'
+                                      //,'js/scripts.js'
+                                     ] */
+      			  
+            }
     		}
-  		}
-		});
+  		},
+      //Minified css
+      cssmin: {
+        combine: {
+          files: {
+            'dist/css/main.min.css': ['css/normalize.css'
+                                     ,'css/base.css'
+                                     ,'css/responsive.css']
+          }
+        }
+      },
+      //copy files and folders (static content)
+      copy: {
+  main: {
+    src: 'static/*',
+    dest: 'dist/',
+  }
+}
 
+    });
 
 	//loading libraries
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
 	//registering tasks
-	grunt.registerTask('default', ['htmlmin','uglify']);
+	grunt.registerTask('default', ['htmlmin','uglify','cssmin','copy']);
 
 
 };
