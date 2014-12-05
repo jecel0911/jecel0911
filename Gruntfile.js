099219caf4d855f89d05263a02db90f9925e90c1
Jeremias Ceciliano <jecel0911@gmail.com>
2014-12-04 22:15:53 -0600

    default project minified and compressed for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,78 @@
 module.exports = function(grunt) {
-  
+
   	//configuration
-	grunt.initConfig({
+   grunt.initConfig({
 		  //Minified HTML
   		htmlmin: {                                     		// Task
     		dist: {                                      	// Target
       			options: {                                 	// Target options
-        			removeComments: true,
-        			collapseWhitespace: true
-      			},
+             removeComments: true,
+             collapseWhitespace: true
+           },
       			files: {                                   	// Dictionary of files
         			'dist/index.html': 'index.html'     	// 'destination': 'source'
         			//,'dist/contact.html': 'demo.html'
-      			}
-    		}
-  		},
+           }
+         }
+       },
       //Minified js
-  		uglify: {
-    		my_target: {
-      			files: {
-        			'dist/js/init.min.js': ['js/init.js'
-        								              ,'js/modernizr.js'
-                                     ]
-             /*,'dist/js/main.min.js': ['js/init.js'
-                                      //,'js/scripts.js'
-                                     ] */
-      			  
-            }
-    		}
-  		},
+      uglify: {
+        my_target: {
+         files: {
+           'dist/js/modernizr.js': ['js/modernizr.js']
+          ,'dist/js/main.min.js': ['js/jquery-2.1.1.min.js'
+                                  ,'assets/owl-carousel/owl.carousel.min.js'
+                                  ,'js/waypoints.js'
+                                  ,'js/jquery.fittext.js'
+                                  ,'js/magnific-popup.js'
+                                  ,'js/wow.min.js'
+                                  ,'js/jquery.nicescroll.min.js'
+                                  ,'js/script.js'
+                                  ,'js/googleanalitycs.js'
+                                  ]
+
+                                    }
+                                  }
+                                },
+      //Clean Unused CSS styles
+      uncss: {
+        dist: {
+          files: {
+            'dist/css/main.min.css': ['index.html' /*, 'app/about.html'*/]
+          }
+        }
+      },
       //Minified css
       cssmin: {
         combine: {
           files: {
-            'dist/css/main.min.css': ['css/normalize.css'
+            'dist/css/main.min.css': [ 'dist/css/main.min.css'/*'css/normalize.css'
                                      //,'css/preLoadingPage.css'
                                      ,'css/base.css'
-                                     ,'css/responsive.css']
-          }
-        }
-      },
+                                     ,'css/responsive.css'*/]
+                                   }
+                                 }
+                               },
       //copy files and folders (static content)
       copy: {
-  main: {
-    src: 'static/*',
-    dest: 'dist/',
-  }
-}
+        main: {
+          expand: true,
+          src: ['static/**','assets/**','images/**'], 
+          dest: 'dist/'
+        }
+      }
 
     });
 
 	//loading libraries
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-uncss');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
 	//registering tasks
-	grunt.registerTask('default', ['htmlmin','uglify','cssmin','copy']);
+	grunt.registerTask('default', ['htmlmin','uglify','uncss','cssmin','copy']);
 
 
 };
